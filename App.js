99b1652c8517be5f6b00fbb1f168276b7a0b3150
htetoozin <htetoo.zin09@gmail.com>
2021-07-01 17:33:57 +0630

    Native image library access

--- App.js
+++ App.js
@@ -1,43 +1,39 @@
-import React, { useState } from 'react';
-import ListItem from './app/components/ListItem';
-import ListingEditScreen from './app/screens/ListingEditScreen';
-import LoginScreen from './app/screens/LoginScreen';
+import React, { useState, useEffect } from 'react';
+import { Button, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as Permissions from 'expo-permissions';
+import Screen from './app/components/Screen';
 
 export default function App() {
-  // const [category, setCategory] = useState(categories[0]);
-  return (
-    // <View style={{ backgroundColor: '#f8f4f4', padding: 20, paddingTop: 100 }}>
-    //   <Card
-    //     title='Red jacket for sale'
-    //     subTitle='$100'
-    //     image={require('./app/assets/jacket.jpg')}
-    //   />
-    // </View>
-    // <Screen>
-    //   {/* <Icon name='email' size={35} backgroundColor='red' iconColor='white' /> */}
-    //   <ListItem
-    //     title='My title'
-    //     // subTitle='My subtitle'
-    //     ImageComponent={<Icon name='email' />}
-    //   />
-    // </Screen>
-    // <ListingsScreen />
-    // <Screen>
-    //   {isNew}
-    //   <Switch value={isNew} onValueChange={(newValue) => setIsNew(newValue)} />
-    // </Screen>
-    // <LoginScreen />
-    // <Screen>
-    //   {/* <AppPicker
-    //     selectedItem={category}
-    //     onSelectItem={(item) => setCategory(item)}
-    //     items={categories}
-    //     icon='apps'
-    //     placeholder='Category'
-    //   />
-    //   <AppTextInput icon='email' placeholder='Email' /> */}
+  const [imageUri, setImageUri] = useState('');
 
-    // </Screen>
-    <ListingEditScreen />
+  const requestPermission = async () => {
+    // const result. Permissions.askAsync(Permissions.CAMERA_ROLL, Permissions.LOCATION)
+
+    // const result = await ImagePicker.requestCameraPermissionsAsync();
+    //console.log('result ', result);
+    const { granted } = await ImagePicker.requestCameraPermissionsAsync();
+    if (!granted) alert('You need to enable permission to access the library!');
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      console.log('select image result ', result);
+      if (!result.cancelled) setImageUri(result.uri);
+    } catch (error) {
+      console.log('error reading an image ', error);
+    }
+  };
+
+  return (
+    <Screen>
+      <Button title='Select Image' onPress={selectImage} />
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+    </Screen>
   );
 }
