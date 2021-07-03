fd4545cb8822ddf48e1f3b7190bac4be84db3723
htetoozin <htetoo.zin09@gmail.com>
2021-07-03 14:15:04 +0630

    Install react navigation

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Image } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
-import * as Permissions from 'expo-permissions';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { Text } from 'react-native';
 import Screen from './app/components/Screen';
-import ImageInputList from './app/components/ImageInputList';
-import ListingDetailsScreen from './app/components/ListingDetailsScreen';
-import ListingEditScreen from './app/screens/ListingEditScreen';
+
+const Tweet = () => {
+  return (
+    <Screen>
+      <Text>Tweet</Text>
+    </Screen>
+  );
+};
+
+const TweetDetail = () => {
+  return (
+    <Screen>
+      <Tweet>Tweet Detail</Tweet>
+    </Screen>
+  );
+};
+
+const Stack = createStackNavigator();
+
+const StackNavigator = () => (
+  <Stack.Navigator initialRouteName='Tweet'>
+    <Stack.Screen name='Tweet' component={Tweet} />
+    <Stack.Screen name='TweetDetail' component={TweetDetail} />
+  </Stack.Navigator>
+);
 
 export default function App() {
-  return <ListingEditScreen />;
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
 }
