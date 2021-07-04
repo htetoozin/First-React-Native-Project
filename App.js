3ce2dd12f64151bb2a497f1dbb350e166b299d1d
htetoozin <htetoo.zin09@gmail.com>
2021-07-04 16:51:25 +0630

    Customizing the navigation theme

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import AuthNavigator from './app/navigation/AuthNavigator';
+import navigationTheme from './app/navigation/navigationTheme';
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={navigationTheme}>
       <AuthNavigator />
     </NavigationContainer>
   );
