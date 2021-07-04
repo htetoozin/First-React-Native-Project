735e632e156d5b2dafaf8d9c24a4d9c852f6c570
htetoozin <htetoo.zin09@gmail.com>
2021-07-04 17:03:50 +0630

    App navigator

---
+++ app/navigation/AppNavigator.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import ListingsScreen from '../screens/ListingsScreen';
+import ListingEditScreen from '../screens/ListingEditScreen';
+import AccountScreen from '../screens/AccountScreen';
+
+const Tab = createBottomTabNavigator();
+
+const AppNavigator = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name='Listings' component={ListingsScreen} />
+      <Tab.Screen name='ListingEdit' component={ListingEditScreen} />
+      <Tab.Screen name='Account' component={AccountScreen} />
+    </Tab.Navigator>
+  );
+};
+
+export default AppNavigator;
