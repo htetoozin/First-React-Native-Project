033cacce1a0d089ca7a1989b5d7e1af0e9f14942
htetoozin <htetoo.zin09@gmail.com>
2021-07-05 13:11:23 +0630

    Beautiful the tabs

--- app/navigation/FeedNavigator.js
+++ app/navigation/FeedNavigator.js
@@ -7,15 +7,14 @@ const Stack = createStackNavigator();
 
 const FeedNavigator = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      mode='modal'
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
       <Stack.Screen name='Listings' component={ListingsScreen} />
-      <Stack.Screen
-        name='ListingDetails'
-        component={ListingDetailsScreen}
-        options={{
-          headerShown: false,
-        }}
-      />
+      <Stack.Screen name='ListingDetails' component={ListingDetailsScreen} />
     </Stack.Navigator>
   );
 };
