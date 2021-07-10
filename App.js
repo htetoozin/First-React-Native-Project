02b187b77cb4945b66c317c61038d8bd6399f874
htetoozin <htetoo.zin09@gmail.com>
2021-07-10 15:21:34 +0630

    Caching request get data

--- App.js
+++ App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import { AsyncStorage, Button } from 'react-native';
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './app/navigation/AppNavigator';
 import navigationTheme from './app/navigation/navigationTheme';
 
 export default function App() {
-  const demo = async () => {
-    try {
-      await AsyncStorage.setItem('person', JSON.stringify({ id: 1 }));
-      const value = await AsyncStorage.getItem('person');
-      const person = JSON.parse('person');
-
-      console.log('person ', person);
-    } catch (error) {}
-  };
-
-  return null;
-  // return (
-  //   <NavigationContainer theme={navigationTheme} mode='modal'>
-  //     <AppNavigator />
-  //   </NavigationContainer>
-  // );
+  return (
+    <NavigationContainer theme={navigationTheme} mode='modal'>
+      <AppNavigator />
+    </NavigationContainer>
+  );
 }
--- app/api/client.js
+++ app/api/client.js
@@ -1,7 +1,23 @@
 import { create } from 'apisauce';
+import cache from '../utility/cache';
 
 const apiClient = create({
   baseURL: 'https://peaceful-sierra-12369.herokuapp.com/api',
 });
+
+const get = apiClient.get;
+
+apiClient.get = async (url, params, axiosConfig) => {
+  const response = await get(url, params, axiosConfig);
+  console.log('response ', response);
+
+  if (response.ok) {
+    cache.store(url, response.data);
+    return response;
+  }
+
+  const data = await cache.get(url);
+  return data ? { ok: true, data } : response;
+};
 
 export default apiClient;
