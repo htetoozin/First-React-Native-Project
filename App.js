170a5210c8fd99bc7f56ba381dc7dac133f09718
htetoozin <htetoo.zin09@gmail.com>
2021-07-10 10:26:11 +0630

    Detecting network status

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { Button } from 'react-native';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import AppNavigator from './app/navigation/AppNavigator';
 import navigationTheme from './app/navigation/navigationTheme';
+import NetInfo, { useNetInfo } from '@react-native-community/netinfo';
 
 export default function App() {
+  // NetInfo.addEventListener((netInfo) => console.log(netInfo));
+
+  const netInfo = useNetInfo();
+  console.log('netInfo ', netInfo);
+
   return (
-    <NavigationContainer theme={navigationTheme} mode='modal'>
-      <AppNavigator />
-    </NavigationContainer>
+    <Button disabled={!netInfo.isInternetReachable} title='network button' />
   );
+  // return (
+  //   <NavigationContainer theme={navigationTheme} mode='modal'>
+  //     <AppNavigator />
+  //   </NavigationContainer>
+  // );
 }
