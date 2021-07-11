2c810c728d477e4ebe823b64648aa9a4516fa301
htetoozin <htetoo.zin09@gmail.com>
2021-07-11 11:33:36 +0630

    Offline notice component

--- App.js
+++ App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './app/navigation/AppNavigator';
 import navigationTheme from './app/navigation/navigationTheme';
+import OfflineNotice from './app/components/OfflineNotice';
 
 export default function App() {
   return (
-    <NavigationContainer theme={navigationTheme} mode='modal'>
-      <AppNavigator />
-    </NavigationContainer>
+    <>
+      <OfflineNotice />
+      <NavigationContainer theme={navigationTheme} mode='modal'>
+        <AppNavigator />
+      </NavigationContainer>
+    </>
   );
 }
