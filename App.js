ab9818d409e3152bb163d530e8a1596f0574ad42
htetoozin <htetoo.zin09@gmail.com>
2021-07-13 10:10:01 +0630

    Login API with JWT

--- App.js
+++ App.js
@@ -3,13 +3,14 @@ import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './app/navigation/AppNavigator';
 import navigationTheme from './app/navigation/navigationTheme';
 import OfflineNotice from './app/components/OfflineNotice';
+import AuthNavigator from './app/navigation/AuthNavigator';
 
 export default function App() {
   return (
     <>
       <OfflineNotice />
       <NavigationContainer theme={navigationTheme} mode='modal'>
-        <AppNavigator />
+        <AuthNavigator />
       </NavigationContainer>
     </>
   );
--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,23 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Image } from 'react-native';
 import * as Yup from 'yup';
+import jwt_decode from 'jwt-decode';
 
 import Screen from '../components/Screen';
-import { AppForm, AppFormField, SubmitButton } from '../components/forms';
+import {
+  ErrorMessage,
+  AppForm,
+  AppFormField,
+  SubmitButton,
+} from '../components/forms';
+import authApi from '../api/auth';
 
 const validationSchema = Yup.object().shape({
   email: Yup.string().required().email().label('Email'),
   password: Yup.string().required().min(4).label('Password'),
 });
 
-function LoginScreen(props) {
+const LoginScreen = () => {
+  const [loginFailed, setLoginFailed] = useState(false);
+
+  const handleSubmit = async ({ email, password }) => {
+    const result = await authApi.login(email, password);
+    if (!result.ok) return setLoginFailed(true);
+    setLoginFailed(false);
+
+    const user = jwt_decode(result.data);
+    console.log('user ', user);
+  };
+
   return (
     <Screen style={styles.container}>
       <Image style={styles.logo} source={require('../assets/logo-red.png')} />
+      <ErrorMessage
+        error='Invalid email and /or password.'
+        visible={loginFailed}
+      />
 
       <AppForm
         initialValues={{ email: '', password: '' }}
-        onSubmit={(values) => console.log(values)}
+        onSubmit={handleSubmit}
         validationSchema={validationSchema}
       >
         <AppFormField
@@ -42,7 +64,7 @@ function LoginScreen(props) {
       </AppForm>
     </Screen>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
